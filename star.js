b316c92cbd577d596be5a52b0a02c2f1a51989d5
diyarravandi <37891849+diyarravandi@users.noreply.github.com>
2018-04-23 15:16:02 -0400

    Create star.js

---
+++ star.js
@@ -0,0 +1,16 @@
+var xPos = 163;
+var yPos = 4;
+var xPos2 = 396;
+var yPos2 = 171;
+
+draw = function() {
+    background(29, 40, 115);
+    fill(255, 242, 0);
+    ellipse(xPos, yPos, 45, 44);
+    ellipse(xPos2, yPos2, 45, 44);
+    
+
+yPos = yPos +1;
+xPos2 = xPos2 -1;
+
+};
